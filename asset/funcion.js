9d03c9260f20af9af9abbbc1430e12d33f972f63
Damylet <mirenadamylet@gmail.com>
2024-07-10 13:31:51 -0400

    funcionando encriptar y desencriptar

---
+++ asset/funcion.js
@@ -0,0 +1,23 @@
+function encryptText() {
+    let inputText = document.getElementById('inputText').value;
+    let encryptedText = inputText
+        .replace(/e/g, 'enter')
+        .replace(/i/g, 'imes')
+        .replace(/a/g, 'ai')
+        .replace(/o/g, 'ober')
+        .replace(/u/g, 'ufat');
+    document.getElementById('outputText').value = encryptedText;
+}
+
+function decryptText() {
+    let inputText = document.getElementById('inputText').value;
+    let decryptedText = inputText
+        .replace(/enter/g, 'e')
+        .replace(/imes/g, 'i')
+        .replace(/ai/g, 'a')
+        .replace(/ober/g, 'o')
+        .replace(/ufat/g, 'u');
+    document.getElementById('outputText').value = decryptedText;
+}
+
+
